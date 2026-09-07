d2d301227e711cb4eec288ec5430bd3b485fd162
Abhijith S <abhijithpgd13@gmail.com>
2026-09-08 00:38:42 +0530

    Initialize Spring Boot project

---
+++ src/main/java/com/abhijith/distributed_queue/DistributedQueueApplication.java
@@ -0,0 +1,13 @@
+package com.abhijith.distributed_queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhijith/distributed_queue/DistributedQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhijith.distributed_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
